f0ca72a70e087b60536900258cb2d881139c58a0
Miraaj Kadam <61474583+miraajkadam@users.noreply.github.com>
2020-06-02 16:00:29 +0530

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function (){
+
+    $.getJSON('images.json',function (data) {
+        var arrItems = []
+        $.each(data,function(index, value){
+            arrItems.push(value)
+        })
+    })
+
+    
+    $('#flipbook').turn({
+        width: 700,
+        height: 500,
+        page: 1,
+        autoCenter: true,
+        duration: 2000
+    })    
+    
+})
+
+// fetch('images.json')
+//         .then(function (response){
+//             return response.json()
+//         })
+
+//         .then(function (data){
+//             appendData(data)
+//         })
+
+//         .catch(function (error){
+//             console.log(error)
+//         })
