6db79f129ddd659ae150b0c4b3eb807d95102b8f
Miraaj Kadam <61474583+miraajkadam@users.noreply.github.com>
2020-06-04 20:00:01 +0530

    prettier now

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,19 @@
-$(document).ready(function (){
+$(document).ready(function() {
+	$.getJSON('images.json', function(data) {
+		var arrItems = [];
+		$.each(data, function(index, value) {
+			arrItems.push(value);
+		});
+	});
 
-    $.getJSON('images.json',function (data) {
-        var arrItems = []
-        $.each(data,function(index, value){
-            arrItems.push(value)
-        })
-    })
-
-    
-    $('#flipbook').turn({
-        width: 700,
-        height: 500,
-        page: 1,
-        autoCenter: true,
-        duration: 2000
-    })    
-    
-})
+	$('#flipbook').turn({
+		width: 700,
+		height: 500,
+		page: 1,
+		autoCenter: true,
+		duration: 2000
+	});
+});
 
 // fetch('images.json')
 //         .then(function (response){
